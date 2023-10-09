9a942766bdbb2c7612025ee679f38f8c42d625b4
shyrwall <sh@keff.org>
2023-10-09 12:22:53 +0200

    .

--- flowtrackd.pl
+++ flowtrackd.pl
@@ -4,9 +4,19 @@ use NetPacket::Ethernet qw(:strip);
 use NetPacket::IP qw(:strip);
 use NetPacket::TCP;
 use Net::RawIP;
+use Data::Dumper;
 
 # Implement Cloudflares TCP Reset Cookies in Perl
 
+my $nic = $ARGV[0]; 
+my $ipset_whitelist = $nic;
+$ipset_whitelist =~ s/\./_/;
+$ipset_whitelist = "shield_whitelist_$ipset_whitelist";
+
+# Flush whitelist
+system qq(/usr/sbin/ipset flush $ipset_whitelist);
+
+my $debug = 0;
 my $err = '';
 
 my %states;
@@ -20,7 +30,7 @@ my %tcp_flags=(FIN => FIN,
                ECE => ECE,
                CWR => CWR);
 
-my $pcap = pcap_open_live("eth0", 74, 0, 1000, \$err);
+my $pcap = pcap_open_live("$nic", 74, 0, 1000, \$err);
 my $filter;
 pcap_compile($pcap, \$filter, "tcp and (tcp[tcpflags] & (tcp-syn|tcp-rst) != 0)", 1, 0);
 pcap_setfilter($pcap, $filter);
@@ -44,25 +54,30 @@ sub callback {
 	my $seqnum = $tcp_data_dec->{seqnum};
 	my %options = $tcp_data_dec->parse_tcp_options;
 
-	if($tcp_flags == 2) {
-		send_packet($src_ip,$dst_ip,$src_port,$dst_port,$winsize,$seqnum,$options{ts},$options{mss});
+	my $epoch = time();
+
+	if($tcp_flags == 2 && ($epoch - $whitelisted{$src_ip}{$dst_ip}{$dst_port}) >= 3600 ) {
+		delete($whitelisted{$src_ip}{$dst_ip}{$dst_port});
+		send_packet($src_ip,$dst_ip,$src_port,$dst_port,$winsize,$seqnum,$options{ts},$options{mss},$epoch);
+		print "Sent invalid cookie to $src_ip\n" if $debug;
 	}
 	if($tcp_flags == 4) {
-		return if $states{$src_ip}{$src_port}{$dst_ip}{$dst_port} != $seqnum;
-		whitelist($src_ip,$src_port,$dst_ip,$dst_port);
-		delete($states{$src_ip}{$src_port}{$dst_ip}{$dst_port});
+		return if $states{$src_ip}{$dst_ip}{$dst_port} != $seqnum;
+		whitelist($src_ip,$dst_ip,$dst_port,$epoch);
+		delete($states{$src_ip}{$dst_ip}{$dst_port});
+		print "Whitelisted $src_ip\n";
 	}
 }
 
 sub whitelist() {
-	my ($src_ip,$src_port,$dst_ip,$dst_port) = @_;
-	system qq(echo ipset add shield_whitelist $src_ip,$src_port,$dst_ip,$dst_port);
+	my ($src_ip,$dst_ip,$dst_port,$epoch) = @_;
+	system qq(ipset add $ipset_whitelist $src_ip,$dst_port,$dst_ip);
+	$whitelisted{$src_ip}{$dst_ip}{$dst_port} = $epoch;
 }
 
 sub send_packet() {
-	my ($src_ip,$dst_ip,$src_port,$dst_port,$winsize,$seqnum,$ts,$mss) = @_;
+	my ($src_ip,$dst_ip,$src_port,$dst_port,$winsize,$seqnum,$ts,$mss,$epoch) = @_;
 	
-	my $epoch = time(); 
 	$n = Net::RawIP->new({
 	                      ip => {
 	                              saddr => $dst_ip,
@@ -83,5 +98,6 @@ sub send_packet() {
 	my $ts = pack ("NN", $epoch, $ts);
 	$n->optset(tcp => { type => [ (2, 4, 8, 1, 3) ], data => [ "$hex_mss", "", $ts, "", "\x07" ] });
 	$n->send;
-	$states{$src_ip}{$src_port}{$dst_ip}{$dst_port} = $seqnum -1;
+	$states{$src_ip}{$dst_ip}{$dst_port} = $seqnum -1;
 }
+
